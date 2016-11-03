bc94b748665b76ed562bca24468ff8378761c068
sammykea <samm0091@stud.kea.dk>
2016-11-03 10:58:36 +0100

    added sout sammy

--- src/Presentation/Main.java
+++ src/Presentation/Main.java
@@ -15,6 +15,8 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        // add sout
+        System.out.println("sammy");
     }
 
 
