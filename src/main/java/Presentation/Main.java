e62484da46dcfc2b63537427b173861bc6181669
sammykea <samm0091@stud.kea.dk>
2016-11-04 12:06:46 +0100

    added resources v2

--- src/main/java/Presentation/Main.java
+++ src/main/java/Presentation/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
