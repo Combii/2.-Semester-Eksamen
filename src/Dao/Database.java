f179603c46c928e3837528e5dfbfd9d11fcb4110
sammykea <samm0091@stud.kea.dk>
2016-11-04 11:04:49 +0100

    added external db
    can connect to externeldatabase and use mysql statements.
    added new table bob and users.

--- src/Dao/Database.java
+++ src/Dao/Database.java
@@ -1,5 +1,6 @@
 package Dao;
 
+import javax.xml.transform.Result;
 import java.sql.*;
 
 /**
@@ -7,11 +8,17 @@ import java.sql.*;
  */
 public class Database {
 
-    private String url = "jdbc:mysql://localhost:3306/";
+    /*private String url = "jdbc:mysql://localhost:3306/";
     private String dbName = "erhvervstest";
     private String driver = "com.mysql.jdbc.Driver";
     private String userName = "root";
-    private String password = "JuboKijeCepd1_";
+    private String password = "JuboKijeCepd1_";*/
+
+    private String url = "jdbc:mysql://sql7.freemysqlhosting.net:3306/";
+    private String dbName = "sql7143001";
+    private String driver = "com.mysql.jdbc.Driver";
+    private String userName = "sql7143001";
+    private String password = "nbvkeyktFX";
 
     public Database() {
     }
@@ -20,11 +27,14 @@ public class Database {
         try {
             Connection connection = DriverManager.getConnection(url + dbName, userName, password);
             Statement statement = connection.createStatement();
-            ResultSet res = statement.executeQuery("SELECT * FROM users");
+            //statement.executeUpdate("CREATE TABLE bob(name VARCHAR(255), lastname VARCHAR(255))");
+            //statement.executeUpdate("INSERT INTO bob (name, lastname) values ('bob', 'bobsen')");
+            ResultSet res = statement.executeQuery("SELECT * FROM bob");
             while (res.next()) {
-                System.out.println(res.getString("userName"));
-                System.out.println(res.getString("password"));
+                System.out.println(res.getString("name"));
+                System.out.println(res.getString("lastname"));
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
