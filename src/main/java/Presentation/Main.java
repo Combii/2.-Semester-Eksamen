2d4d4548d82e50f462846138cc6c3c0346712cf2
Combii <davidstovle@gmail.com>
2016-11-22 22:45:47 +0100

    Resized Login and changed main to direct to new Login.fxml

--- src/main/java/Presentation/Main.java
+++ src/main/java/Presentation/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Login");
+        Parent root = FXMLLoader.load(getClass().getResource("/Login/Login.fxml"));
+        primaryStage.setTitle("Another CC Login");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
