918373f3ee0158bdf8e4646ac27b4742d5a609ae
bori0066 <borisgrunwald@gmail.com>
2016-11-25 13:47:45 +0100

    Updated to new database

--- src/main/java/Dao/Database.java
+++ src/main/java/Dao/Database.java
@@ -10,23 +10,13 @@ public class Database {
     private String userName = "sql7146226";
     private String password = "5hrN2C6eYs";
 
-    public Database() {
+    private Database() {
     }
 
-    public void getUsers() {
-        try {
-            Connection connection = DriverManager.getConnection(url + dbName, userName, password);
-            Statement statement = connection.createStatement();
-            //statement.executeUpdate("CREATE TABLE bob(name VARCHAR(255), lastname VARCHAR(255))");
-            //statement.executeUpdate("INSERT INTO bob (name, lastname) values ('bob', 'bobsen')");
-            ResultSet res = statement.executeQuery("SELECT * FROM bob");
-            while (res.next()) {
-                System.out.println(res.getString("name"));
-                System.out.println(res.getString("lastname"));
-            }
+    public Connection getConnection() throws SQLException {
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        Connection connection = DriverManager.getConnection(url+dbName,userName,password);
+
+        return connection;
     }
 }
