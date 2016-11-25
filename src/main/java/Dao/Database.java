c8be480ee69b485e79ae8129fbaf072fd1ea0f53
Combii <davidstovle@gmail.com>
2016-11-25 14:02:14 +0100

    Changed Database.java to be singleton

--- src/main/java/Dao/Database.java
+++ src/main/java/Dao/Database.java
@@ -2,21 +2,32 @@ package Dao;
 
 import java.sql.*;
 
-public class Database {
+public class Database{
 
-    private String url = "jdbc:mysql://sql7.freemysqlhosting.net:3306/";
-    private String dbName = "sql7146226";
-    private String driver = "com.mysql.jdbc.Driver";
-    private String userName = "sql7146226";
-    private String password = "5hrN2C6eYs";
+    private static SingletonDatabase database = null;
 
-    private Database() {
+    //private constructor to avoid client applications to use constructor
+    private Database(){}
+
+    public static SingletonDatabase getDatabase(){
+        if(database == null){
+            database = new SingletonDatabase();
+        }
+        return database;
     }
 
-    public Connection getConnection() throws SQLException {
+    private static class SingletonDatabase {
+        private String url = "jdbc:mysql://sql7.freemysqlhosting.net:3306/";
+        private String dbName = "sql7146226";
+        private String driver = "com.mysql.jdbc.Driver";
+        private String userName = "sql7146226";
+        private String password = "5hrN2C6eYs";
 
-        Connection connection = DriverManager.getConnection(url+dbName,userName,password);
 
-        return connection;
+        public Connection getConnection() throws SQLException {
+
+            Connection connection = DriverManager.getConnection(url+dbName,userName,password);
+            return connection;
+        }
     }
 }
