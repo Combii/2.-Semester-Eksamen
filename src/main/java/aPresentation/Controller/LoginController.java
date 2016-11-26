e51720e4e2d1bec857149f1db9e481b58432158d
Combii <davidstovle@gmail.com>
2016-11-26 16:35:21 +0100

    Changed Login.fxml
    Text now says if login typed is incorrect

--- src/main/java/aPresentation/Controller/LoginController.java
+++ src/main/java/aPresentation/Controller/LoginController.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -23,25 +24,35 @@ public class LoginController {
     public TextField username;
     public TextField password;
     public Button loginButton;
+    public Text textOverLoginButton;
 
     public void clickedOnLoginButton(ActionEvent actionEvent) throws HashCode.CannotPerformOperationException, SQLException, HashCode.InvalidHashException, IOException {
-        int number =  UserValidation.isUser(username.getText(), password.getText());
-        System.out.println(number);
+        try {
+            int number = UserValidation.isUser(username.getText(), password.getText());
 
-        Stage stage = null;
-        Parent root = null;
+            Stage stage = null;
+            Parent root = null;
 
 
-        if(number == 0){
-
-            stage =(Stage) loginButton.getScene().getWindow();
-            //load up OTHER FXML document
-            root = FXMLLoader.load(getClass().getResource("/Admin Task/Browse Menu/BrowseMenu.fxml"));
+            if (number == 0) {
+                stage = (Stage) loginButton.getScene().getWindow();
+                //load up OTHER FXML document
+                root = FXMLLoader.load(getClass().getResource("/Admin Task/Browse Menu/BrowseMenu.fxml"));
+                //create a new scene with root and set the stage
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
+            else if(number == -1){
+                textOverLoginButton.setText("Username or Password is incorrect");
+            }
+            else if(number == -2){
+                textOverLoginButton.setText("Password typed does not match to username");
+            }
         }
-        //create a new scene with root and set the stage
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        catch (NullPointerException e){
+            textOverLoginButton.setText("Username or Password is incorrect");
+        }
 
     }
 
@@ -52,4 +63,5 @@ public class LoginController {
     public void rememberMeCheckBox(ActionEvent actionEvent) {
 
     }
+
 }
