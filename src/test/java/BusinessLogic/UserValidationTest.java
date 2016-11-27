a9805c6213fedcf61155607f9601360e5b3d2fc6
jikol1906 <borisgrunwald@gmail.com>
2016-11-27 11:11:16 +0100

    Added password and username validation + test (#20)

---
+++ src/test/java/BusinessLogic/UserValidationTest.java
@@ -0,0 +1,31 @@
+package BusinessLogic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ${Boris} Grunwald} on 27/11/2016.
+ */
+public class UserValidationTest {
+    @Test
+    public void isUser() throws Exception {
+
+    }
+
+    @Test
+    public void isValidUsername() throws Exception {
+
+    }
+
+    @Test
+    public void isValidPassword() throws Exception {
+
+    }
+
+    @Test
+    public void startConnectionToDB() throws Exception {
+
+    }
+
+}
