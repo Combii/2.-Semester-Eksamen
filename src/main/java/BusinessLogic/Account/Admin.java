f212284f7523bcd115d4e0d7bcbc49613a8248ca
jikol1906 <borisgrunwald@gmail.com>
2016-11-29 22:28:13 +0100

    DAOinterface (#36)
    * Commit so i can update from master
    
    * Can now use save, exists and delete in AccountDAO

--- src/main/java/BusinessLogic/Account/Admin.java
+++ src/main/java/BusinessLogic/Account/Admin.java
@@ -4,16 +4,9 @@ package BusinessLogic.Account;
  * Created by David Stovlbaek
  * 29 November 2016.
  */
-public class Admin extends Account {
-
-    private String username;
-    private String email;
-    private String lastName;
+public class Admin extends Administrator {
 
     public Admin(String username, String password, int userType, String name, String lastName, String email) {
-        super(name, password, userType);
-        this.username = username;
-        this.email = email;
-        this.lastName = lastName;
+        super(username, password, userType, name, lastName, email);
     }
 }
--- src/main/java/Dao/AccountDao.java
+++ src/main/java/Dao/AccountDao.java
@@ -1,6 +1,13 @@
 package Dao;
 
 
+
+import BusinessLogic.Account.Account;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +26,7 @@ public class AccountDao {
         conn = d.getConnection();
     }
 
-    public boolean userExists(String username) throws SQLException{
+    public boolean userExists(String username) throws SQLException {
         PreparedStatement ps = conn.prepareStatement("SELECT EXISTS(SELECT * FROM Account WHERE username = '" + username + "');");
         ResultSet rs = ps.executeQuery();
         rs.next();
@@ -37,8 +44,7 @@ public class AccountDao {
             String rString = rs.getString(1);
             closeStatementAndResultsetAndConnection();
             return rString;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return null;
         }
     }
@@ -74,12 +80,16 @@ public class AccountDao {
     }
 
     // Need to close ResultSet, PreparedStatement and connection after use
-    private void closeStatementAndResultsetAndConnection(){
-        try { rs.close(); } catch (Exception e) { /* ignored */ }
-        try { ps.close(); } catch (Exception e) { /* ignored */ }
+    private void closeStatementAndResultsetAndConnection() {
+        try {
+            rs.close();
+        } catch (Exception e) { /* ignored */ }
+        try {
+            ps.close();
+        } catch (Exception e) { /* ignored */ }
     }
 
-    public List getUser() throws SQLException{
+    public List getUser() throws SQLException {
         try {
             PreparedStatement ps = conn.prepareStatement("SELECT ID, username FROM Account");
             ResultSet rs = ps.executeQuery();
@@ -87,20 +97,19 @@ public class AccountDao {
             int columnCount = md.getColumnCount();
             List<List<String>> list = new ArrayList<List<String>>();
 
-            while(rs.next()){
+            while (rs.next()) {
                 List<String> row = new ArrayList<String>(columnCount);
                 int i = 1;
-                while(i <= columnCount){
+                while (i <= columnCount) {
                     row.add(rs.getString(i++));
                 }
                 list.add(row);
             }
             closeStatementAndResultsetAndConnection();
             return list;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return null;
         }
     }
-
 }
+
