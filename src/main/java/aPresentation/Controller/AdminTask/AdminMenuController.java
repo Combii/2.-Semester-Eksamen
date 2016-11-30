0c57482f3d69dabebb6c0735b677e621ed5b0d0c
sammykea <samm0091@stud.kea.dk>
2016-11-30 11:06:25 +0100

    added dialogbox to handledeletebutton

--- src/main/java/aPresentation/Controller/AdminTask/AdminMenuController.java
+++ src/main/java/aPresentation/Controller/AdminTask/AdminMenuController.java
@@ -2,6 +2,7 @@ package aPresentation.Controller.AdminTask;
 
 import BusinessLogic.EditAdminBLogic;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
@@ -48,10 +49,27 @@ public class AdminMenuController {
     @FXML
     private void handleDeleteButton() throws SQLException, IOException {
         eab = new EditAdminBLogic();
+
         try {
-            eab.deleteAdmin(firstName.getText());
+            if (eab.deleteAdmin(firstName.getText())) {
+                Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+                alert2.setTitle("Information Dialog");
+                alert2.setHeaderText(null);
+                alert2.setContentText(firstName.getText() + " has been deleted");
+                alert2.showAndWait();
+            }
+
+            else {
+                Alert alert3 = new Alert(Alert.AlertType.INFORMATION);
+                alert3.setTitle("Information Dialog");
+                alert3.setHeaderText(null);
+                alert3.setContentText("User doesn't exist");
+                alert3.showAndWait();
+            }
         }
-        catch (Exception e) {
+        catch (Exception e)
+
+        {
             e.printStackTrace();
         }
     }
