8205e1c971cdc1ec33085b175c5a3735a31a2f8d
Combii <davidstovle@gmail.com>
2016-11-30 12:33:15 +0100

    Small fix in DropboxDB

--- src/main/java/Dao/DropboxDatabase.java
+++ src/main/java/Dao/DropboxDatabase.java
@@ -11,13 +11,13 @@ import com.dropbox.core.v2.users.FullAccount;
 /**
  * Created by David Stovlbaek
  * 30 November 2016.
- *     //https://www.dropbox.com/developers/documentation/java#tutorial
+ * https://www.dropbox.com/developers/documentation/java#tutorial
  */
 public class DropboxDatabase {
 
     private static final String ACCESS_TOKEN = "dripAUbSyiAAAAAAAAAACZtuHT06tu2vhZi1cjb1beSfaSEq7osLYkUBmKwcSDLY";
     private static DbxClientV2 client = null;
-    private static DropboxDatabase dropboxDatabase = null;
+        private static DropboxDatabase dropboxDatabase = null;
 
 
     //private constructor to avoid client applications to use constructor
