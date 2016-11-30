eb3795a7845fd0b1a863260d676102088202dccf
Combii <davidstovle@gmail.com>
2016-11-30 23:42:43 +0100

    Added File Class and removed unused imports

--- src/main/java/aPresentation/Controller/AdminTask/ShowAccountsController.java
+++ src/main/java/aPresentation/Controller/AdminTask/ShowAccountsController.java
@@ -1,25 +1,14 @@
 package aPresentation.Controller.AdminTask;
 
 import BusinessLogic.Account.UserInformation;
-import Dao.AccountDao;
-import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
-import javafx.collections.transformation.SortedList;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.sql.SQLException;
-
-import static javafx.application.Application.launch;
 
 /**
  * Created by Lenovo on 28-11-2016.
