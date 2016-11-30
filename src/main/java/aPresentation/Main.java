0a537ef75281c97c0d63974410854a1da1104889
Combii <davidstovle@gmail.com>
2016-11-30 13:32:56 +0100

    Fixed Main

--- src/main/java/aPresentation/Main.java
+++ src/main/java/aPresentation/Main.java
@@ -1,6 +1,5 @@
 package aPresentation;
 
-import Dao.DropboxDatabase;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -22,8 +21,6 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-       // launch(args);
-        DropboxDatabase dropboxDatabase = DropboxDatabase.getDropboxDB();
-        dropboxDatabase.getFile("/sup");
+       launch(args);
     }
 }
