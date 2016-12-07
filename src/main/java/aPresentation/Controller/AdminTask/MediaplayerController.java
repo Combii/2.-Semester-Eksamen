2f030e17723d437cfab08122209e12ea9cfdb470
cmt95 <cihat_2650@hotmail.com>
2016-12-07 10:07:35 +0100

    deleted unused field

--- src/main/java/aPresentation/Controller/AdminTask/MediaplayerController.java
+++ src/main/java/aPresentation/Controller/AdminTask/MediaplayerController.java
@@ -34,9 +34,7 @@ public class MediaplayerController implements Initializable {
     Slider volumeSlider;
     @FXML
     Slider timeSlider;
-
-    private boolean atEndOfMedia = false;
-
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         String path = new File("C:\\Users\\Lenovo\\IdeaProjects\\2.-Semester-Eksamen\\src\\main\\java\\Media\\COSTA RICA IN 4K 60fps (ULTRA HD) w Freefly Movi.mp4").getAbsolutePath();
