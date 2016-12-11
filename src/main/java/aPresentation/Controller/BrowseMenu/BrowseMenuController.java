f324fb9a6d48e9a72ad3263c475645f2da09471a
bori0066 <borisgrunwald@gmail.com>
2016-12-11 20:46:08 +0100

    Can now add and remove folders

--- src/main/java/aPresentation/Controller/BrowseMenu/BrowseMenuController.java
+++ src/main/java/aPresentation/Controller/BrowseMenu/BrowseMenuController.java
@@ -2,24 +2,29 @@ package aPresentation.Controller.BrowseMenu;
 
 import BusinessLogic.File.FileStorage;
 import Dao.FilePath;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.cell.TextFieldTreeCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
 
+
+import java.awt.*;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.awt.*;
 import java.io.IOException;
-
 
 
 /**
@@ -29,44 +34,59 @@ import java.io.IOException;
 public class BrowseMenuController implements Initializable {
     public AnchorPane splitPane;
     public GridPane gridPane;
+    public ContextMenu contextMenu;
 
     @FXML
     TreeView<String> treeView;
 
-    Image icon = new Image("/img/folder.png",20,20,false,false);
+    Image icon = new Image("/img/folder.png", 20, 20, false, false);
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        treeView.setEditable(true);
+        treeView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         TreeItem<String> root = new TreeItem<>("Root", new ImageView(icon));
         root.setExpanded(true);
 
-        TreeItem<String> nodeA = new TreeItem<>("node A", new ImageView(icon));
-        TreeItem<String> nodeB = new TreeItem<>("node B", new ImageView(icon));
-        TreeItem<String> nodeC = new TreeItem<>("node C", new ImageView(icon));
-        root.getChildren().addAll(nodeA,nodeB,nodeC);
-        nodeA.setExpanded(true);
+        contextMenu = new ContextMenu();
 
-        TreeItem<String> nodeA1 = new TreeItem<>("node A 1", new ImageView(icon));
-        TreeItem<String> nodeB1 = new TreeItem<>("node B 1", new ImageView(icon));
-        TreeItem<String> nodeC1 = new TreeItem<>("node C 1", new ImageView(icon));
-        nodeA.getChildren().addAll(nodeA1,nodeB1,nodeC1);
+
+
+        MenuItem newF = new MenuItem("Add folder");
+        MenuItem del = new MenuItem("Delete folder");
+        MenuItem ren = new MenuItem("Rename");
+
+        contextMenu.getItems().addAll(newF, del,ren);
+        treeView.setContextMenu(contextMenu);
+
+        //New Folder pressed
+        newF.setOnAction(e -> {
+            if(treeView.getRoot() == null ) {
+                treeView.setRoot(new TreeItem<>("New Folder", new ImageView(icon)));
+            } else {
+                TreeItem<String> t = treeView.getSelectionModel().getSelectedItem();
+                t.getChildren().add(new TreeItem<>("New Folder", new ImageView(icon)));
+            }
+        });
+
+        //Delete folder pressed
+        del.setOnAction(e -> {
+                TreeItem<String> t = treeView.getSelectionModel().getSelectedItem();
+                if(t == root) treeView.setRoot(null);
+                else t.getParent().getChildren().remove(t);
+        });
 
         treeView.setRoot(root);
         setGridPane("pics");
     }
-    public void mouseClick(javafx.scene.input.MouseEvent mouseEvent){
-        if(mouseEvent.getClickCount() == 2) {
-            TreeItem<String> item = treeView.getSelectionModel().getSelectedItem();
-            System.out.println(item.getValue());
-        }
-    }
 
-    private void setGridPane(String dropboxFolderPath){
+
+    private void setGridPane(String dropboxFolderPath) {
         try {
             FileStorage list = new FileStorage();
             list.downloadFilesToList(dropboxFolderPath);
 
             int rowCounter = 0, columnCounter = 0;
-
 
 
             for (final FilePath i : list.getList()) {
@@ -81,8 +101,7 @@ public class BrowseMenuController implements Initializable {
                             //https://stackoverflow.com/questions/5824916/how-do-i-open-an-image-in-the-default-image-viewer-using-java-on-windows
                             File file = new File(i.getLocalPath());
                             Desktop.getDesktop().open(file);
-                        }
-                        catch (IOException e){
+                        } catch (IOException e) {
                             e.printStackTrace();
                         }
                     }
@@ -104,8 +123,7 @@ public class BrowseMenuController implements Initializable {
                     rowCounter++;
                 }
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
