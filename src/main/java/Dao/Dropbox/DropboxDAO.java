d2eab3c656a25e8b1bc60a6f8843a2d8d51fe776
Combii <davidstovle@gmail.com>
2016-12-12 12:51:58 +0100

    FolderDAO (#69)
    * Created FolderDAO
    
    * Save and delete now works + changed save(int) to save(string)
    
    * Fixed get
    
    * added save again
    
    * Fixed return list

--- src/main/java/Dao/Dropbox/DropboxDAO.java
+++ src/main/java/Dao/Dropbox/DropboxDAO.java
@@ -190,9 +190,6 @@ public class DropboxDAO implements DAO<List<FilePath>> {
     }
 
     @Override
-    public void delete(int id) throws SQLException {
-    }
-
     public void delete(String dropBoxPath) {
         try{
         ps = conn.prepareStatement("DELETE FROM FilePathDropboxDB WHERE path = '" + dropBoxPath + "');");
---
+++ src/main/java/Dao/Dropbox/FolderDAO.java
@@ -0,0 +1,61 @@
+package Dao.Dropbox;
+
+import Dao.DAO;
+import Dao.SQLDatabase;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ${Boris} Grunwald} on 12/12/2016.
+ */
+public class FolderDAO {
+
+    private Connection conn;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+
+    public FolderDAO() throws SQLException {
+
+        conn = SQLDatabase.getDatabase().getConnection();
+
+    }
+
+    public void save(String folderName) throws SQLException {
+
+        ps = conn.prepareStatement("INSERT INTO Folder (folderName) VALUE '"+folderName+"';");
+        ps.executeUpdate();
+
+    }
+
+    public List<String> getFolders() throws SQLException {
+        List<String> rList = new ArrayList<>();
+
+        PreparedStatement ps = conn.prepareStatement("SELECT * FROM Folder;");
+        ResultSet rs = ps.executeQuery();
+        return rList;
+    }
+
+    }
+
+
+    public void delete(String name) throws SQLException {
+        ps = conn.prepareStatement("DELETE FROM Folder WHERE folderName = " + name);
+        ps.executeUpdate();
+        closeStatementAndResultsetAndConnection();
+    }
+
+    // Need to close ResultSet, PreparedStatement and connection after use
+    private void closeStatementAndResultsetAndConnection() {
+        try {
+            rs.close();
+        } catch (Exception e) { /* ignored */ }
+        try {
+            ps.close();
+        } catch (Exception e) { /* ignored */ }
+    }
+}
